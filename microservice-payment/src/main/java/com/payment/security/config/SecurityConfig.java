ba675d50b3ef5e574807f2c19bdbed47092f4ff9
filipemartinsdev <dev.filipemartins@gmail.com>
2026-04-07 15:42:06 -0300

    feat: setup project
    Configure Caddy (Reverse Proxy/API Gateway), RabbitMQ connection, Prometheus Scrapes and Grafana connection

---
+++ microservice-payment/src/main/java/com/payment/security/config/SecurityConfig.java
@@ -0,0 +1,32 @@
+package com.payment.security.config;
+
+import jakarta.servlet.DispatcherType;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(csrf -> csrf.disable())
+
+                .cors(cors -> cors.configure(http))
+
+                .sessionManagement(sessionManagement -> sessionManagement
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+
+                .authorizeHttpRequests(auth -> auth
+                        .dispatcherTypeMatchers(DispatcherType.ERROR).permitAll()
+                        .requestMatchers("/v3/api-docs/**", "/swagger-ui/index.html", "/h2-console/**").permitAll()
+                        .requestMatchers("/api/v1/register", "/api/v1/login", "/api/v1/refresh").permitAll()
+                        .requestMatchers("/actuator/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+
+                .build();
+    }
+}

ea3e87d6ed5a593f2513b02fc0be476fac300776
filipemartinsdev <dev.filipemartins@gmail.com>
2026-04-09 13:37:04 -0300

    feat(notification): setup resource server for notification microservice

--- microservice-notification/src/main/java/com/notification/security/config/SecurityConfig.java
+++ microservice-notification/src/main/java/com/notification/security/config/SecurityConfig.java
@@ -1,7 +1,9 @@
 package com.notification.security.config;
 
 import jakarta.servlet.DispatcherType;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
@@ -9,6 +11,7 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 public class SecurityConfig {
 
+    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
                 .csrf(csrf -> csrf.disable())
@@ -22,9 +25,12 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .dispatcherTypeMatchers(DispatcherType.ERROR).permitAll()
                         .requestMatchers("/v3/api-docs/**", "/swagger-ui/index.html", "/h2-console/**").permitAll()
-                        .requestMatchers("/api/v1/register", "/api/v1/login", "/api/v1/refresh").permitAll()
                         .requestMatchers("/actuator/**").permitAll()
                         .anyRequest().authenticated()
+                )
+
+                .oauth2ResourceServer(oauth -> oauth
+                        .jwt(Customizer.withDefaults())
                 )
 
                 .build();

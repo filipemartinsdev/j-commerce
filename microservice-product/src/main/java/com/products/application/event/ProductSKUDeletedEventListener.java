866698a81935ed1a995573ffdb99818aa295c95e
filipemartinsdev <dev.filipemartins@gmail.com>
2026-04-14 14:24:32 -0300

    fix(product): listen ProductSKUDeletedEvent to cascade soft-delete on ProductSKUPrice

--- microservice-product/src/main/java/com/products/application/event/ProductSKUDeletedEventListener.java
+++ microservice-product/src/main/java/com/products/application/event/ProductSKUDeletedEventListener.java
@@ -1,22 +1,28 @@
 package com.products.application.event;
 
+import com.products.application.service.AdminProductPriceService;
 import com.products.application.service.AdminProductService;
 import com.products.application.service.ProductStockService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ApplicationListener;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 public class ProductSKUDeletedEventListener implements ApplicationListener<ProductSKUDeletedEvent> {
     private final ProductStockService productStockService;
+    private final AdminProductPriceService adminProductPriceService;
 
-    public ProductSKUDeletedEventListener(ProductStockService productStockService) {
+    public ProductSKUDeletedEventListener(ProductStockService productStockService, AdminProductPriceService adminProductPriceService) {
         this.productStockService = productStockService;
+        this.adminProductPriceService = adminProductPriceService;
     }
 
     @Override @Async
     public void onApplicationEvent(ProductSKUDeletedEvent event) {
         productStockService.deleteByProductSKUId(event.getProductSKUId());
+        adminProductPriceService.deleteAllByProductSKUId(event.getProductSKUId());
     }
 
     @Override

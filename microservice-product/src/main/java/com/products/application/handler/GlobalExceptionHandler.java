e58f728b13bebb7bfc1da69239e3053e5eb8a8b4
filipemartinsdev <dev.filipemartins@gmail.com>
2026-04-14 12:39:20 -0300

    feat(product): implement GlobalExceptionHandler to handle product management exception

---
+++ microservice-product/src/main/java/com/products/application/handler/GlobalExceptionHandler.java
@@ -0,0 +1,77 @@
+package com.products.application.handler;
+
+import com.products.application.dto.StandardResponse;
+import com.products.application.exception.*;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@Slf4j
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<StandardResponse<Void>> handleException(Exception e) {
+        log.error(e.getMessage(), e);
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(StandardResponse.error(e.getMessage()));
+    }
+
+    @ExceptionHandler(ProductNotFoundException.class)
+    public ResponseEntity<StandardResponse<Void>> handleProductNotFound(ProductNotFoundException e) {
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(StandardResponse.fail(e.getMessage()));
+    }
+
+    @ExceptionHandler(ProductSKUPriceNotFoundException.class)
+    public ResponseEntity<StandardResponse<Void>> handleProductSKUPriceNotFound(ProductSKUPriceNotFoundException e) {
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(StandardResponse.fail(e.getMessage()));
+    }
+
+    @ExceptionHandler(ProductSKUNotFoundException.class)
+    public ResponseEntity<StandardResponse<Void>> handleProductSKUNotFound(ProductSKUNotFoundException e) {
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(StandardResponse.fail(e.getMessage()));
+    }
+
+    @ExceptionHandler(ProductStockNotFoundException.class)
+    public ResponseEntity<StandardResponse<Void>> handleProductStockNotFound(ProductStockNotFoundException e) {
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(StandardResponse.fail(e.getMessage()));
+    }
+
+    @ExceptionHandler(ProductNotActiveException.class)
+    public ResponseEntity<StandardResponse<Void>> handleProductNotActive(ProductNotActiveException e) {
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(StandardResponse.fail(e.getMessage()));
+    }
+
+    @ExceptionHandler(InvalidProductPriceTypeException.class)
+    public ResponseEntity<StandardResponse<Void>> handleInvalidProductPriceType(ProductNotActiveException e) {
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(StandardResponse.fail(e.getMessage()));
+    }
+
+    @ExceptionHandler(InvalidProductCategoryException.class)
+    public ResponseEntity<StandardResponse<Void>> handleInvalidProductCategory(InvalidProductCategoryException e) {
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(StandardResponse.fail(e.getMessage()));
+    }
+
+    @ExceptionHandler(CantDeleteProductException.class)
+    public ResponseEntity<StandardResponse<Void>> handleCantDeleteProduct(CantDeleteProductException e) {
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(StandardResponse.fail(e.getMessage()));
+    }
+}

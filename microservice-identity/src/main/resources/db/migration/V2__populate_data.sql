81f64ed02012d053d35d1417348eec4252ac167b
filipemartinsdev <dev.filipemartins@gmail.com>
2026-04-18 12:56:12 -0300

    feat(identity): create migration V2 to register default ADMIN user

---
+++ microservice-identity/src/main/resources/db/migration/V2__populate_data.sql
@@ -0,0 +1,26 @@
+INSERT INTO user_credentials (user_id, email, encrypted_password, first_name, last_name, role, created_at, is_active)
+VALUES(
+    gen_random_uuid(),
+    'admin@gmail.com',
+    '$2a$10$WhEbY/14jwqrMrzS0dIUGeb839nF9GaYbnstoWWyG1a0.xbvOgH5K',
+    'admin',
+    'test',
+    'ADMIN',
+    CURRENT_TIMESTAMP,
+    TRUE
+);
+
+INSERT INTO user_profile (user_id, email, first_name, last_name, created_at, is_active)
+VALUES (
+    (
+        SELECT u.user_id
+        FROM user_credentials u
+        WHERE u.role = 'ADMIN'
+        LIMIT 1
+    ),
+    'admin@gmail.com',
+    'admin',
+    'test',
+    CURRENT_TIMESTAMP,
+    TRUE
+);

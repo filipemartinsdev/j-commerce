6ef3827c641edeb416c80a4d53bbb6f5cae990f4
filipemartinsdev <dev.filipemartins@gmail.com>
2026-04-23 14:24:25 -0300

    fix(product): configure MessageBroker to user JSON serializer instead of standard java Binary objects

---
+++ microservice-product/src/main/java/com/products/config/MessageBrokerConfig.java
@@ -0,0 +1,24 @@
+package com.products.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MessageBrokerConfig {
+    @Value("${broker.queues.shoppingCartConfirmation.name}")
+    private String SHOPPING_CART_CONFIRMATION_QUEUE_NAME;
+
+    @Bean
+    public Queue shoppingCartConfirmationQueue() {
+        return new Queue(SHOPPING_CART_CONFIRMATION_QUEUE_NAME, true);
+    }
+
+    @Bean
+    public Jackson2JsonMessageConverter jackson2JsonMessageConverter() {
+        return new Jackson2JsonMessageConverter(new ObjectMapper());
+    }
+}

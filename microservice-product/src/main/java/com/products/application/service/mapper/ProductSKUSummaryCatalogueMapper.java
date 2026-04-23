1fe61c2801a979a2967e56b09686446fc0a0942b
filipemartinsdev <dev.filipemartins@gmail.com>
2026-04-23 14:26:46 -0300

    feat(product): create mappers to ShoppingCart DTOs and fix mapper of productSKUSummary to don't use null values

--- microservice-product/src/main/java/com/products/application/service/mapper/ProductSKUSummaryCatalogueMapper.java
+++ microservice-product/src/main/java/com/products/application/service/mapper/ProductSKUSummaryCatalogueMapper.java
@@ -3,6 +3,7 @@ package com.products.application.service.mapper;
 import com.products.application.dto.catalogue.ProductPriceCatalogueResponse;
 import com.products.application.dto.catalogue.ProductSKUSummaryCatalogueResponse;
 import com.products.application.dto.StockStatus;
+import com.products.domain.entity.PriceType;
 import com.products.domain.entity.ProductSKUSummaryCatalogue;
 import org.springframework.stereotype.Component;
 
@@ -18,9 +19,9 @@ public class ProductSKUSummaryCatalogueMapper {
                 StockStatus.fromStockCount(entity.getStockCount()),
                 new ProductPriceCatalogueResponse(
                         entity.getOriginalPrice(),
-                        entity.getCurrentPrice() == null ? BigDecimal.ZERO : entity.getCurrentPrice(),
-                        discountPercent,
-                        entity.getCurrentPriceTypeName()
+                        entity.getCurrentPrice() == null ? entity.getOriginalPrice() : entity.getCurrentPrice(),
+                        discountPercent == null ? 0 : discountPercent,
+                        entity.getCurrentPriceTypeName() == null ? PriceType.Value.COMMON.getName() : entity.getCurrentPriceTypeName()
                 )
         );
     }
--- microservice-product/src/main/java/com/products/application/service/mapper/ShoppingCartItemMapper.java
+++ microservice-product/src/main/java/com/products/application/service/mapper/ShoppingCartItemMapper.java
@@ -1,5 +1,6 @@
 package com.products.application.service.mapper;
 
+import com.products.application.dto.ShoppingCartConfirmationItem;
 import com.products.application.dto.catalogue.ShoppingCartItemResponse;
 import com.products.application.service.ProductDiscountCalculator;
 import com.products.domain.entity.ShoppingCartItemProductSKUResume;
@@ -22,11 +23,20 @@ public class ShoppingCartItemMapper {
                 entity.getProductSKUName(),
                 entity.getUnits(),
                 entity.getOriginalPrice(),
-                entity.getCurrentPrice() == null ? BigDecimal.ZERO : entity.getCurrentPrice(),
+                entity.getCurrentPrice() == null ? entity.getOriginalPrice() : entity.getCurrentPrice(),
                 productDiscountCalculator.getDiscountPercent(
                         entity.getOriginalPrice(),
                         entity.getCurrentPrice()
                 )
+        );
+    }
+
+    public ShoppingCartConfirmationItem toShoppingCartConfirmation(ShoppingCartItemProductSKUResume entity) {
+        return new ShoppingCartConfirmationItem(
+                entity.getProductSKUId(),
+                entity.getProductSKUName(),
+                entity.getUnits(),
+                entity.getCurrentPrice() == null ? entity.getOriginalPrice() : entity.getCurrentPrice()
         );
     }
 }

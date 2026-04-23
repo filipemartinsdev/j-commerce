b49f9f640b645a5baddb4b2da210971379257a79
filipemartinsdev <dev.filipemartins@gmail.com>
2026-04-23 14:46:36 -0300

    feat(order): create infrastructure layer to manage REST requests for manage sales orders and delivery addresses

---
+++ microservice-order/src/main/java/com/orders/infra/web/DeliveryAddressController.java
@@ -0,0 +1,95 @@
+package com.orders.infra.web;
+
+import com.orders.application.dto.*;
+import com.orders.application.service.DeliveryAddressService;
+import com.orders.domain.entity.DeliveryAddress;
+import jakarta.validation.Valid;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/v1/delivery-addresses")
+public class DeliveryAddressController {
+    private final DeliveryAddressService deliveryAddressService;
+
+    public DeliveryAddressController(DeliveryAddressService deliveryAddressService) {
+        this.deliveryAddressService = deliveryAddressService;
+    }
+
+    @GetMapping
+    public ResponseEntity<StandardResponse<PagedResponse<DeliveryAddressResponse>>> getAllAddressesByUser(
+            @AuthenticationPrincipal Jwt jwt,
+            Pageable pageable
+    ) {
+        UUID authenticatedUserId = UUID.fromString(jwt.getSubject());
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(StandardResponse.success(deliveryAddressService.getAllByUserId(
+                        authenticatedUserId, pageable
+                )));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<StandardResponse<DeliveryAddressResponse>> getAllAddressesByUser(
+            @PathVariable UUID id,
+            @AuthenticationPrincipal Jwt jwt
+    ) {
+        UUID authenticatedUserId = UUID.fromString(jwt.getSubject());
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(StandardResponse.success(
+                        deliveryAddressService.getById(id, authenticatedUserId)
+                ));
+    }
+
+    @PostMapping
+    public ResponseEntity<StandardResponse<DeliveryAddressResponse>> createAddress(
+            @Valid @RequestBody CreateDeliveryAddressRequest request,
+            @AuthenticationPrincipal Jwt jwt
+    ) {
+        UUID authenticatedUserId = UUID.fromString(jwt.getSubject());
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(StandardResponse.success(
+                        deliveryAddressService.createByUserId(request, authenticatedUserId)
+                ));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<StandardResponse<DeliveryAddressResponse>> createAddress(
+            @PathVariable UUID id,
+            @AuthenticationPrincipal Jwt jwt
+    ) {
+        UUID authenticatedUserId = UUID.fromString(jwt.getSubject());
+        deliveryAddressService.deleteById(id, authenticatedUserId);
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .build();
+
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<StandardResponse<DeliveryAddressResponse>> createAddress(
+            @PathVariable UUID id,
+            @Valid @RequestBody UpdateDeliveryAddressRequest request,
+            @AuthenticationPrincipal Jwt jwt
+    ) {
+        UUID authenticatedUserId = UUID.fromString(jwt.getSubject());
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(StandardResponse.success(
+                        deliveryAddressService.updateById(id, authenticatedUserId, request)
+                ));
+    }
+}
---
+++ microservice-order/src/main/java/com/orders/infra/web/SalesOrderController.java
@@ -0,0 +1,46 @@
+package com.orders.infra.web;
+
+import com.orders.application.dto.*;
+import com.orders.application.service.SalesOrderService;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/v1/sales-orders")
+public class SalesOrderController {
+    private final SalesOrderService salesOrderService;
+
+    public SalesOrderController(SalesOrderService salesOrderService) {
+        this.salesOrderService = salesOrderService;
+    }
+
+    @GetMapping
+    public ResponseEntity<StandardResponse<PagedResponse<SalesOrderResponse>>> getAllSalesOrders(
+            @AuthenticationPrincipal Jwt jwt,
+            Pageable pageable
+    ) {
+        UUID authenticatedUserId = UUID.fromString(jwt.getSubject());
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(StandardResponse.success(salesOrderService.getAllByUserId(authenticatedUserId, pageable)));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<StandardResponse<SalesOrderSummaryResponse>> getSalesOrderById(
+            @AuthenticationPrincipal Jwt jwt,
+            @PathVariable UUID id
+    ) {
+        UUID authenticatedUserId = UUID.fromString(jwt.getSubject());
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(StandardResponse.success(salesOrderService.getSummaryById(id, authenticatedUserId)));
+    }
+}

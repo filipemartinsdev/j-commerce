e089537e09ff34e67eac8e51e7c690f503ecd2f8
filipemartinsdev <dev.filipemartins@gmail.com>
2026-04-23 14:13:19 -0300

    feat(product): enable Feign Clients

--- microservice-product/src/main/java/com/products/MicroserviceProductsApplication.java
+++ microservice-product/src/main/java/com/products/MicroserviceProductsApplication.java
@@ -2,9 +2,10 @@ package com.products;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableAsync;
 
-@SpringBootApplication @EnableAsync
+@SpringBootApplication @EnableAsync @EnableFeignClients
 public class MicroserviceProductsApplication {
 
 	public static void main(String[] args) {

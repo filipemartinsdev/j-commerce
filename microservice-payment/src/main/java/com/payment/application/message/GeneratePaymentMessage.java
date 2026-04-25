487376136eea70dbabc10adaff6493d2954157a5
filipemartinsdev <dev.filipemartins@gmail.com>
2026-04-24 22:15:34 -0300

    feat(payment): create messages to manage order payment

---
+++ microservice-payment/src/main/java/com/payment/application/message/GeneratePaymentMessage.java
@@ -0,0 +1,12 @@
+package com.payment.application.message;
+
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+public record GeneratePaymentMessage (
+        UUID orderId,
+        UUID userId,
+        BigDecimal totalAmount
+) {
+}
---
+++ microservice-payment/src/main/java/com/payment/application/message/PaymentConfirmedMessage.java
@@ -0,0 +1,4 @@
+package com.payment.application.message;
+
+public record PaymentConfirmedMessage() {
+}

03bbde2cc15f3d605414af39313d9b154423ca15
filipemartinsdev <dev.filipemartins@gmail.com>
2026-04-24 22:11:40 -0300

    feat(product): finish message broker configuration

--- microservice-order/src/main/java/com/orders/config/BrokerConfig.java
+++ microservice-order/src/main/java/com/orders/config/BrokerConfig.java
@@ -11,12 +11,36 @@ import org.springframework.context.annotation.Profile;
 @Configuration
 @Profile("!test")
 public class BrokerConfig {
-    @Value("${broker.queues.shoppingCartConfirmation.name}")
-    private String SHOPPING_CART_CONFIRMATION_QUEUE_NAME;
+    @Value("${broker.queues.createOrder.name}")
+    private String CREATE_ORDER_QUEUE_NAME;
+
+    @Value("${broker.queues.generatePayment.name}")
+    private String GENERATE_PAYMENT_QUEUE_NAME;
+
+    @Value("${broker.queues.handlePaymentTimeout.name}")
+    private String HANDLE_PAYMENT_TIMEOUT_QUEUE_NAME;
+
+    @Value("${broker.queues.confirmOrderPayment.name}")
+    private String CONFIRM_ORDER_PAYMENT_QUEUE_NAME;
 
     @Bean
-    public Queue shoppingCartConfirmationQueue() {
-        return new Queue(SHOPPING_CART_CONFIRMATION_QUEUE_NAME, true);
+    public Queue createOrderQueue() {
+        return new Queue(CREATE_ORDER_QUEUE_NAME, true);
+    }
+
+    @Bean
+    public Queue generatePaymentQueue() {
+        return new Queue(GENERATE_PAYMENT_QUEUE_NAME, true);
+    }
+
+    @Bean
+    public Queue handlePaymentTimeoutQueue() {
+        return new Queue(HANDLE_PAYMENT_TIMEOUT_QUEUE_NAME, true);
+    }
+
+    @Bean
+    public Queue confirmOrderPaymentQueue() {
+        return new Queue(CONFIRM_ORDER_PAYMENT_QUEUE_NAME, true);
     }
 
     @Bean

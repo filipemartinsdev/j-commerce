fcb51ec8dc1b6222dce599eba3c356627d563022
filipemartinsdev <dev.filipemartins@gmail.com>
2026-05-07 17:36:29 -0300

    feat(identity): create PagedResponseFactory

---
+++ microservice-identity/src/main/java/com/identity/common/exception/InvalidEntityMapperException.java
@@ -0,0 +1,7 @@
+package com.identity.common.exception;
+
+public class InvalidEntityMapperException extends RuntimeException {
+    public InvalidEntityMapperException(String message) {
+        super(message);
+    }
+}
---
+++ microservice-identity/src/main/java/com/identity/common/exception/NullResponsePageException.java
@@ -0,0 +1,7 @@
+package com.identity.common.exception;
+
+public class NullResponsePageException extends RuntimeException {
+    public NullResponsePageException(String message) {
+        super(message);
+    }
+}

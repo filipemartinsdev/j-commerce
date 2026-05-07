e6c62153eb26625004e14fbc30e8db6576cd76d3
filipemartinsdev <dev.filipemartins@gmail.com>
2026-05-07 17:32:18 -0300

    feat(notification): implement PagedResponseFactory

---
+++ microservice-notification/src/main/java/com/notification/application/exception/InvalidEntityMapperException.java
@@ -0,0 +1,7 @@
+package com.notification.application.exception;
+
+public class InvalidEntityMapperException extends RuntimeException {
+    public InvalidEntityMapperException(String message) {
+        super(message);
+    }
+}
---
+++ microservice-notification/src/main/java/com/notification/application/exception/NullResponsePageException.java
@@ -0,0 +1,7 @@
+package com.notification.application.exception;
+
+public class NullResponsePageException extends RuntimeException {
+    public NullResponsePageException(String message) {
+        super(message);
+    }
+}

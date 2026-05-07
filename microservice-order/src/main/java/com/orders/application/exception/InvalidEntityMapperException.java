1406c27f89e7c1b8aff9c5297397ff0a8b124e8c
filipemartinsdev <dev.filipemartins@gmail.com>
2026-05-07 19:43:59 -0300

    feat(order): create PagedResponseFactory and its exceptions

---
+++ microservice-order/src/main/java/com/orders/application/exception/InvalidEntityMapperException.java
@@ -0,0 +1,7 @@
+package com.orders.application.exception;
+
+public class InvalidEntityMapperException extends RuntimeException {
+    public InvalidEntityMapperException(String message) {
+        super(message);
+    }
+}
---
+++ microservice-order/src/main/java/com/orders/application/exception/NullResponsePageException.java
@@ -0,0 +1,7 @@
+package com.orders.application.exception;
+
+public class NullResponsePageException extends RuntimeException {
+    public NullResponsePageException(String message) {
+        super(message);
+    }
+}

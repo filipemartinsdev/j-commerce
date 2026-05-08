2e264eaf743eae44c4c69d0202c8c7d6b424b472
filipemartinsdev <dev.filipemartins@gmail.com>
2026-05-08 12:53:02 -0300

    fix(identity): remove cache configuration^Crom test profile

--- microservice-identity/src/main/java/com/identity/config/CacheConfig.java
+++ microservice-identity/src/main/java/com/identity/config/CacheConfig.java
@@ -2,10 +2,12 @@ package com.identity.config;
 
 import com.github.benmanes.caffeine.cache.Caffeine;
 import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.caffeine.CaffeineCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -13,7 +15,7 @@ import org.springframework.data.redis.connection.RedisConnectionFactory;
 import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 
-@Configuration
+@Configuration @EnableCaching @Profile("!test")
 public class CacheConfig {
     @Bean @Primary
     public CacheManager redisCacheManager(RedisConnectionFactory redisConnectionFactory) {

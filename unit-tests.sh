48e88a539e977bdd6d2daec3606bca6092299316
filipemartinsdev <dev.filipemartins@gmail.com>
2026-05-08 11:24:03 -0300

    test: create shell script to run all unit testes

---
+++ unit-tests.sh
@@ -0,0 +1,14 @@
+cd microservice-identity
+mvn clean test
+
+cd ../microservice-product
+mvn clean test
+
+cd ../microservice-order
+mvn clean test
+
+cd ../microservice-payment
+mvn clean test
+
+cd ../microservice-notification
+mvn clean test

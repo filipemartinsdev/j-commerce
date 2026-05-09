1d9db0d76f695c34dd2fc3f3cd140aaa7196aa03
filipemartinsdev <dev.filipemartins@gmail.com>
2026-05-09 19:34:09 -0300

    test(product): create Integration Tests for ShoppingCartController

---
+++ microservice-product/src/test/java/com/products/infra/web/catalogue/ShoppingCartControllerTests.java
@@ -0,0 +1,302 @@
+package com.products.infra.web.catalogue;
+
+import com.products.application.dto.PagedResponse;
+import com.products.application.dto.catalogue.ConfirmShoppingCartRequest;
+import com.products.application.dto.catalogue.CreateShoppingCartItemRequest;
+import com.products.application.dto.catalogue.ShoppingCartItemResponse;
+import com.products.application.exception.*;
+import com.products.application.service.ShoppingCartService;
+import com.products.config.SecurityConfig;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.UUID;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.jwt;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(ShoppingCartController.class)
+@Import(SecurityConfig.class)
+public class ShoppingCartControllerTests {
+    @MockitoBean private ShoppingCartService shoppingCartService;
+
+    @Autowired private MockMvc mockMvc;
+
+    @WithMockUser(authorities = "SCOPE_USER")
+    @Test @DisplayName("Should retrieve shopping cart items and return status code 200")
+    void getAllItemsTestCase1() throws Exception {
+        ShoppingCartItemResponse itemResponse = new ShoppingCartItemResponse(
+                UUID.randomUUID(),
+                UUID.randomUUID(),
+                "testing",
+                1,
+                BigDecimal.ONE,
+                BigDecimal.ONE,
+                0
+        );
+
+        String expectedJSON = """
+            {
+                "data": {
+                    "page": 0,
+                    "size": 20,
+                    "totalPages": 1,
+                    "totalElements": 1,
+                    "isLast": true,
+                    "content": [
+                        {
+                            "id": "%s",
+                            "productSKUId": "%s",
+                            "productSKUName": "testing",
+                            "units": 1,
+                            "originalPrice": 1,
+                            "currentPrice": 1,
+                            "discountPercent": 0
+                        }
+                    ]
+                },
+                "status": "success"
+            }
+        """.formatted(itemResponse.id(), itemResponse.productSKUId());
+
+        when(shoppingCartService.getAllItems(any(), any()))
+                .thenReturn(
+                        PagedResponse.<ShoppingCartItemResponse>builder()
+                                .page(0)
+                                .size(20)
+                                .totalElements(1L)
+                                .totalPages(1)
+                                .isLast(true)
+                                .content(List.of(itemResponse))
+                                .build()
+                );
+
+        mockMvc.perform(
+                get("/api/v1/shopping-cart")
+                        .with(jwt().jwt(jwt -> jwt.subject(UUID.randomUUID().toString())))
+                )
+                .andExpect(status().isOk())
+                .andExpect(content().json(expectedJSON));
+    }
+
+    @Test @DisplayName("Should return status code 401 if client is not authenticated")
+    void getAllItemsTestCase2() throws Exception {
+        mockMvc.perform(get("/api/v1/shopping-cart"))
+                .andExpect(status().isUnauthorized());
+    }
+
+
+    @WithMockUser(authorities = "SCOPE_USER")
+    @Test @DisplayName("Should create new shopping cart item and return status code 201")
+    void createTestCase1() throws Exception {
+        UUID productSKUId = UUID.randomUUID();
+        String requestBody = """
+            {
+                "productSKUId": "%s",
+                "units": 2
+            }
+        """.formatted(productSKUId);
+
+        doNothing().when(shoppingCartService).createItemByUserId(any(), any());
+
+        mockMvc.perform(post("/api/v1/shopping-cart")
+                .content(requestBody)
+                .contentType(MediaType.APPLICATION_JSON)
+                .with(jwt().jwt(jwt -> jwt.subject(UUID.randomUUID().toString())))
+        ).andExpect(status().isCreated());
+
+        verify(shoppingCartService).createItemByUserId(any(CreateShoppingCartItemRequest.class), any());
+    }
+
+    @Test @DisplayName("Should return status code 401 if client is not authenticated")
+    void createTestCase2() throws Exception {
+        String requestBody = """
+            {
+                "productSKUId": "%s",
+                "units": 2
+            }
+        """.formatted(UUID.randomUUID());
+
+        mockMvc.perform(post("/api/v1/shopping-cart")
+                .content(requestBody)
+                .contentType(MediaType.APPLICATION_JSON)
+        ).andExpect(status().isUnauthorized());
+    }
+
+    @WithMockUser(authorities = "SCOPE_USER")
+    @Test @DisplayName("Should return status code 404 if product not exists")
+    void createTestCase3() throws Exception {
+        UUID productSKUId = UUID.randomUUID();
+        String requestBody = """
+            {
+                "productSKUId": "%s",
+                "units": 2
+            }
+        """.formatted(productSKUId);
+
+        doThrow(ProductSKUNotFoundException.class)
+                .when(shoppingCartService).createItemByUserId(any(), any());
+
+        mockMvc.perform(post("/api/v1/shopping-cart")
+                .content(requestBody)
+                .contentType(MediaType.APPLICATION_JSON)
+                .with(jwt().jwt(jwt -> jwt.subject(UUID.randomUUID().toString())))
+        ).andExpect(status().isNotFound());
+    }
+
+    @WithMockUser(authorities = "SCOPE_USER")
+    @Test @DisplayName("Should return status code 422 if product haven't stock enough")
+    void createTestCase4() throws Exception {
+        UUID productSKUId = UUID.randomUUID();
+        String requestBody = """
+            {
+                "productSKUId": "%s",
+                "units": 9999
+            }
+        """.formatted(productSKUId);
+
+        doThrow(ProductOutOfStockException.class)
+                .when(shoppingCartService).createItemByUserId(any(), any());
+
+        mockMvc.perform(post("/api/v1/shopping-cart").with(jwt().jwt(jwt -> jwt.subject(UUID.randomUUID().toString())))
+                .content(requestBody)
+                .contentType(MediaType.APPLICATION_JSON)
+        ).andExpect(status().isUnprocessableContent());
+    }
+
+    @WithMockUser(authorities = "SCOPE_USER")
+    @Test @DisplayName("Should return status code 400 if product is already on shopping cart")
+    void createTestCase5() throws Exception {
+        UUID productSKUId = UUID.randomUUID();
+        String requestBody = """
+            {
+                "productSKUId": "%s",
+                "units": 2
+            }
+        """.formatted(productSKUId);
+
+        doThrow(ShoppingCartItemAlreadyExistsException.class)
+                .when(shoppingCartService).createItemByUserId(any(), any());
+
+        mockMvc.perform(post("/api/v1/shopping-cart").with(jwt().jwt(jwt -> jwt.subject(UUID.randomUUID().toString())))
+                .content(requestBody)
+                .contentType(MediaType.APPLICATION_JSON)
+        ).andExpect(status().isBadRequest());
+    }
+
+
+    @WithMockUser(authorities = "SCOPE_USER")
+    @Test @DisplayName("Should clean shopping cart and return status code 200")
+    void deleteAllItemsTestCase1() throws Exception {
+        doNothing().when(shoppingCartService).deleteAllItemsByUserId(any());
+
+        mockMvc.perform(delete("/api/v1/shopping-cart").with(jwt().jwt(jwt -> jwt.subject(UUID.randomUUID().toString()))))
+                .andExpect(status().isOk());
+
+        verify(shoppingCartService).deleteAllItemsByUserId(any());
+    }
+
+    @Test @DisplayName("Should return status code 401 if client is not authenticated")
+    void deleteAllItemsTestCase2() throws Exception {
+        mockMvc.perform(delete("/api/v1/shopping-cart"))
+                .andExpect(status().isUnauthorized());
+    }
+
+    @WithMockUser(authorities = "SCOPE_USER")
+    @Test @DisplayName("Should remove item from shopping cart and return status code 200")
+    void deleteByIdTestCase1() throws Exception {
+        UUID itemId = UUID.randomUUID();
+
+        doNothing().when(shoppingCartService).deleteItemById(any(), any());
+
+        mockMvc.perform(delete("/api/v1/shopping-cart/" + itemId).with(jwt().jwt(jwt -> jwt.subject(UUID.randomUUID().toString()))))
+                .andExpect(status().isOk());
+
+        verify(shoppingCartService).deleteItemById(any(), any());
+    }
+
+    @WithMockUser(authorities = "SCOPE_USER")
+    @Test @DisplayName("Should return status code 404 if item not exists")
+    void deleteByIdTestCase2() throws Exception {
+        UUID itemId = UUID.randomUUID();
+
+        doThrow(ShoppingCartItemNotFoundException.class)
+                .when(shoppingCartService).deleteItemById(any(), any());
+
+        mockMvc.perform(delete("/api/v1/shopping-cart/" + itemId).with(jwt().jwt(jwt -> jwt.subject(UUID.randomUUID().toString()))))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test @DisplayName("Should return status code 401 if client is not authenticated")
+    void deleteByIdTestCase3() throws Exception {
+        mockMvc.perform(delete("/api/v1/shopping-cart/" + UUID.randomUUID()))
+                .andExpect(status().isUnauthorized());
+    }
+
+    @WithMockUser(authorities = "SCOPE_USER")
+    @Test @DisplayName("Should confirm shopping cart and return status code 201")
+    void confirmTestCase1() throws Exception {
+        UUID deliveryAddressId = UUID.randomUUID();
+        String requestBody = """
+            {
+                "deliveryAddressId": "%s"
+            }
+        """.formatted(deliveryAddressId);
+
+        doNothing().when(shoppingCartService).confirmShoppingCart(any(), any(), any());
+
+        mockMvc.perform(post("/api/v1/shopping-cart/checkout")
+                .content(requestBody)
+                .contentType(MediaType.APPLICATION_JSON)
+                .with(jwt().jwt(jwt -> jwt.subject(UUID.randomUUID().toString())))
+        ).andExpect(status().isCreated());
+
+        verify(shoppingCartService).confirmShoppingCart(any(ConfirmShoppingCartRequest.class), any(), any());
+    }
+
+    @WithMockUser(authorities = "SCOPE_USER")
+    @Test @DisplayName("Should return status code 404 if delivery address not exists")
+    void confirmTestCase2() throws Exception {
+        UUID deliveryAddressId = UUID.randomUUID();
+        String requestBody = """
+            {
+                "deliveryAddressId": "%s"
+            }
+        """.formatted(deliveryAddressId);
+
+        doThrow(DeliveryAddressNotFoundException.class)
+                .when(shoppingCartService).confirmShoppingCart(any(), any(), any());
+
+        mockMvc.perform(post("/api/v1/shopping-cart/checkout")
+                .content(requestBody)
+                .contentType(MediaType.APPLICATION_JSON)
+                .with(jwt().jwt(jwt -> jwt.subject(UUID.randomUUID().toString())))
+        ).andExpect(status().isNotFound());
+    }
+
+    @Test @DisplayName("Should return status code 401 if client is not authenticated")
+    void confirmTestCase3() throws Exception {
+        String requestBody = """
+            {
+                "deliveryAddressId": "%s"
+            }
+        """.formatted(UUID.randomUUID());
+
+        mockMvc.perform(post("/api/v1/shopping-cart/checkout")
+                .content(requestBody)
+                .contentType(MediaType.APPLICATION_JSON)
+        ).andExpect(status().isUnauthorized());
+    }
+}

7b0727fb313e55d8a30f5d3adf8b319775ac49a8
filipemartinsdev <dev.filipemartins@gmail.com>
2026-05-14 15:11:18 -0300

    fix(order): create RequestInterceptor for NominatimClient to handle standard headers instead of application.yaml configuration

--- microservice-order/src/main/java/com/orders/config/NominatimFeignClientConfig.java
+++ microservice-order/src/main/java/com/orders/config/NominatimFeignClientConfig.java
@@ -1,5 +1,6 @@
 package com.orders.config;
 
+import feign.RequestInterceptor;
 import feign.codec.ErrorDecoder;
 import org.springframework.context.annotation.Bean;
 
@@ -7,5 +8,13 @@ public class NominatimFeignClientConfig {
     @Bean
     public ErrorDecoder errorDecoder() {
         return new NominatimFeignClientErrorDecoder();
+    }
+
+    @Bean
+    public RequestInterceptor requestInterceptor() {
+        return requestTemplate -> {
+            requestTemplate.header("User-Agent", "JCommerce/1.0 (dev.filipemartins@gmail.com)");
+            requestTemplate.header("Referer", "https://github.com/filipemartinsdev/j-commerce");
+        };
     }
 }
--- microservice-order/src/main/java/com/orders/config/NominatimFeignClientErrorDecoder.java
+++ microservice-order/src/main/java/com/orders/config/NominatimFeignClientErrorDecoder.java
@@ -3,8 +3,12 @@ package com.orders.config;
 import com.orders.application.exception.BadGatewayException;
 import com.orders.application.exception.InvalidDeliveryAddressCoordinatesException;
 import feign.Response;
+import feign.Util;
 import feign.codec.ErrorDecoder;
 import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 @Slf4j
 public class NominatimFeignClientErrorDecoder implements ErrorDecoder {
@@ -13,8 +17,14 @@ public class NominatimFeignClientErrorDecoder implements ErrorDecoder {
         return switch (response.status()){
             case 404, 400 -> new InvalidDeliveryAddressCoordinatesException("Invalid coordinates");
             default -> {
-                log.error("Nominatim invalid response with status code {}: {}", response.status(), response.body());
-                yield new BadGatewayException("Nominatim Bad Gateway with status code " + response.status());
+                try {
+                    String bodyString = Util.toString(response.body().asReader(StandardCharsets.UTF_8));
+                    log.error("Nominatim invalid response with status code {}: {}", response.status(), bodyString);
+                } catch (IOException e) {
+                    log.error("Error reading response body", e);
+                } finally {
+                    yield new BadGatewayException("Nominatim invalid response with status code " + response.status());
+                }
             }
         };
     }

de5766d4e9db3f919f76e30a65209c128712aee5
filipemartinsdev <dev.filipemartins@gmail.com>
2026-05-14 12:55:13 -0300

    feat(order): create Feign Client for GraphHopper

---
+++ microservice-order/src/main/java/com/orders/application/service/GraphHopperClient.java
@@ -0,0 +1,28 @@
+package com.orders.application.service;
+
+import com.orders.application.dto.GeocodingResponse;
+import com.orders.application.dto.RouteRequest;
+import com.orders.application.dto.RouteResponse;
+import com.orders.config.GraphHopperFeignClientConfig;
+import jakarta.validation.Valid;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(name = "graphHopperClient", url = "https://https://graphhopper.com", configuration = GraphHopperFeignClientConfig.class)
+public interface GraphHopperClient {
+    @PostMapping("/api/1/route")
+    ResponseEntity<RouteResponse> route(
+            @RequestParam String key,
+            @Valid @RequestBody RouteRequest body
+    );
+
+    @GetMapping("/api/1/geocode")
+    ResponseEntity<GeocodingResponse> geocode(
+            @RequestParam String key,
+            @RequestParam String q
+    );
+}

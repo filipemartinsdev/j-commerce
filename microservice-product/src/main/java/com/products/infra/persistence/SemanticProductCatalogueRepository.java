c3cc3e6ee7768f66c49c9235bbe54c4a62868445
filipemartinsdev <dev.filipemartins@gmail.com>
2026-05-26 12:33:16 -0300

    feat(product): create repository to use catalogue semantic search view

---
+++ microservice-product/src/main/java/com/products/infra/persistence/SemanticProductCatalogueRepository.java
@@ -0,0 +1,30 @@
+package com.products.infra.persistence;
+
+import com.products.domain.entity.ProductResumeCatalogue;
+import com.products.domain.entity.SemanticProductCatalogue;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface SemanticProductCatalogueRepository extends JpaRepository<SemanticProductCatalogue, UUID> {
+    @Query(
+            nativeQuery = true,
+
+            value =
+            """
+            SELECT * FROM semantic_product_catalogue p
+            ORDER BY p.embedding <-> CAST(:query AS VECTOR)
+            """,
+
+            countQuery =
+            """
+            SELECT COUNT(*) FROM semantic_product_catalogue p
+            """
+    )
+    Page<SemanticProductCatalogue> findAll(@Param("query") float[] query, Pageable pageable);
+}

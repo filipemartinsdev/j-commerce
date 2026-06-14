81e31a47abb1ccb7a646bf498d5e10fa2e0229d6
filipemartinsdev <dev.filipemartins@gmail.com>
2026-06-14 12:28:41 -0300

    fix(identity): remove database seeder config from test environment

--- microservice-identity/src/main/java/com/identity/config/DatabaseSeeder.java
+++ microservice-identity/src/main/java/com/identity/config/DatabaseSeeder.java
@@ -11,13 +11,14 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Slf4j
-@Configuration
+@Configuration @Profile("!test")
 public class DatabaseSeeder {
     private final UserCredentialsRepository userCredentialsRepository;
     private final UserProfileRepository userProfileRepository;

0032e12b82f066c5eee111d67d010b547d3807cb
filipemartinsdev <dev.filipemartins@gmail.com>
2026-06-23 11:01:55 -0300

    feat(identity): handle DataAccessResourceFailureException

--- microservice-identity/src/main/java/com/identity/common/handler/GlobalExceptionHandler.java
+++ microservice-identity/src/main/java/com/identity/common/handler/GlobalExceptionHandler.java
@@ -8,6 +8,7 @@ import com.identity.security.application.exception.UserAlreadyExistsException;
 import com.identity.security.application.exception.UserNotFoundException;
 import io.github.responsekit.core.StandardResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DataAccessResourceFailureException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.oauth2.jwt.BadJwtException;
@@ -17,14 +18,13 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @Slf4j
 @ControllerAdvice
 public class GlobalExceptionHandler {
-
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<StandardResponse<Void>> handleException(Exception exception){
+    public ResponseEntity<Void> handleException(Exception exception){
         log.error(exception.getMessage(), exception);
 
         return ResponseEntity
                 .status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body(StandardResponse.error().message(exception.getMessage()).build());
+                .build();
     }
 
     @ExceptionHandler(BadJwtException.class)
@@ -32,6 +32,13 @@ public class GlobalExceptionHandler {
         return ResponseEntity
                 .status(HttpStatus.BAD_REQUEST)
                 .body(StandardResponse.fail().message(exception.getMessage()).build());
+    }
+
+    @ExceptionHandler(DataAccessResourceFailureException.class)
+    public ResponseEntity<Void> handleException(DataAccessResourceFailureException exception){
+        log.warn("Database connection timeout: {}", exception.getMessage());
+
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
     }
 
     @ExceptionHandler(UserNotFoundException.class)

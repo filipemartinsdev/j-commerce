1574bcb630c7955071bebeb07ce4b487e3de7dc4
filipemartinsdev <dev.filipemartins@gmail.com>
2026-06-25 22:13:54 -0300

    fix(order): add correct type ID to repository

--- microservice-order/src/main/java/com/orders/infra/persistence/SalesOrderItemRepository.java
+++ microservice-order/src/main/java/com/orders/infra/persistence/SalesOrderItemRepository.java
@@ -1,7 +1,9 @@
 package com.orders.infra.persistence;
 
 import com.orders.domain.entity.SalesOrderItem;
+import com.orders.domain.entity.SalesOrderItemId;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface SalesOrderItemRepository extends JpaRepository<SalesOrderItem, Long> {
+public interface SalesOrderItemRepository extends JpaRepository<SalesOrderItem, SalesOrderItemId> {
 }

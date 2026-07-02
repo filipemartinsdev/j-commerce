471e64a45ab8908a3d805e46fef4c09b8cc321fa
filipemartinsdev <dev.filipemartins@gmail.com>
2026-07-02 16:07:00 -0300

    fix(product): remove invalid TODOs

--- microservice-product/src/main/java/com/products/application/exception/CursorDecodingException.java
+++ microservice-product/src/main/java/com/products/application/exception/CursorDecodingException.java
@@ -1,6 +1,5 @@
 package com.products.application.exception;
 
-// TODO: handle this
 public class CursorDecodingException extends RuntimeException {
     public CursorDecodingException(String message) {
         super(message);
--- microservice-product/src/main/java/com/products/application/exception/CursorEncodingException.java
+++ microservice-product/src/main/java/com/products/application/exception/CursorEncodingException.java
@@ -1,6 +1,5 @@
 package com.products.application.exception;
 
-// TODO: handle this
 public class CursorEncodingException extends RuntimeException {
     public CursorEncodingException(String message) {
         super(message);

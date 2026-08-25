c177241b60b32e7af966fb134263e1b705b08b8e
filipemartinsdev <dev.filipemartins@gmail.com>
2026-08-25 17:34:41 -0300

    docs(pricing): add comment to explain UnitOfWork interface

--- microservice-pricing/src/main/kotlin/com/pricing/application/UnitOfWork.kt
+++ microservice-pricing/src/main/kotlin/com/pricing/application/UnitOfWork.kt
@@ -1,5 +1,9 @@
 package com.pricing.application
 
+/**
+ * Abstraction to represent an atomic operation, hiding details about ACID from Framework.
+ * An execution is atomic because if any error occurs, the entire operation is roll backed.
+ */
 interface UnitOfWork {
     fun <T> execute(block: () -> T): T;
 }

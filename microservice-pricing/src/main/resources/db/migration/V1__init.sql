c8bc491335743e1458b33255033ae6b51cbeca3d
filipemartinsdev <dev.filipemartins@gmail.com>
2026-08-25 10:30:35 -0300

    chore(pricing): update SQL schema columns

--- microservice-pricing/src/main/resources/db/migration/V1__init.sql
+++ microservice-pricing/src/main/resources/db/migration/V1__init.sql
@@ -1,26 +1,26 @@
-CREATE TABLE sku_mirror (
+CREATE TABLE product (
     id          UUID PRIMARY KEY,
     sku         VARCHAR(255) NOT NULL UNIQUE,
     deleted     BOOLEAN NOT NULL DEFAULT FALSE
 );
 
-CREATE INDEX sku_mirror_sku_idx
-    ON sku_mirror(sku)
+CREATE INDEX product_sku_idx
+    ON product(sku)
     WHERE deleted IS FALSE;
 
 CREATE TABLE price (
     id              UUID PRIMARY KEY,
-    sku_mirror_id   UUID NOT NULL REFERENCES sku_mirror(id),
+    product_id      UUID NOT NULL REFERENCES product(id),
+    type_id         INTEGER NOT NULL DEFAULT 1,
     value           DECIMAL(19, 2) NOT NULL,
     since           TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
     until           TIMESTAMP WITH TIME ZONE,
     active          BOOLEAN NOT NULL DEFAULT FALSE,
-    type            VARCHAR(50) NOT NULL DEFAULT 'COMMON',
     created_at      TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
     created_by      UUID NOT NULL,
     deleted         BOOLEAN NOT NULL DEFAULT FALSE
 );
 
-CREATE INDEX price_sku_mirror_id_idx
-    ON price(sku_mirror_id)
+CREATE INDEX price_product_id_idx
+    ON price(product_id)
     WHERE deleted IS FALSE;

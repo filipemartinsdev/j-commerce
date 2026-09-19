4e330ab7198f51bed8f1542cf163af9450790769
filipemartinsdev <dev.filipemartins@gmail.com>
2026-09-19 17:42:23 -0300

    test(product): configure test container for Floci

--- microservice-product/src/test/java/com/products/MicroserviceProductsApplicationTests.java
+++ microservice-product/src/test/java/com/products/MicroserviceProductsApplicationTests.java
@@ -1,11 +1,17 @@
 package com.products;
 
+import io.floci.testcontainers.FlociContainer;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest @ActiveProfiles("test")
+@Import(TestContainerConfigs.class)
 class MicroserviceProductsApplicationTests {
+	@Autowired
+	public FlociContainer flociContainer;
 
 	@Test
 	void contextLoads() {
